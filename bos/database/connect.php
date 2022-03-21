d1154827f50f67ecc3d1a41dcc445b6cfe4cd906
Nack Thanaphon <78144742+nackdev2535@users.noreply.github.com>
2022-03-22 00:59:58 +0700

    db

--- bos/database/connect.php
+++ bos/database/connect.php
@@ -2,13 +2,13 @@
 session_start();
 date_default_timezone_set('Asia/Bangkok');
 $servername = "localhost";
-$username = "aunhpnac_aunhpn";
-$password = "f6e64gq6";
+$username = "root";
+$password = "";
 // mughorth_mugh
 // f6e64gq6
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=aunhpnac_aunhpn", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=mugh", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
